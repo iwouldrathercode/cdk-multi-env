348704d29d6ca29fed3ea11e6b8124fb012c6a68
Shankar Ganesh <psgganesh@gmail.com>
2024-07-18 21:03:47 +1000

    logging and destroy commands

--- utils.ts
+++ utils.ts
@@ -12,5 +12,13 @@ export class InfrastructureContext {
 
   public load() {
     dotenv.config({ path: this.envFile });
+    console.log(`Git Branch ${this.currentBranch}`);
+    console.log(`Loading configuration from ${this.envFile}`);
+    console.log(`Git Branch ${process.env.BRANCH}`);
+    console.log(`Environment ${process.env.ENVIRONMENT}`);
+    console.log(`Profile ${process.env.AWS_PROFILE}`);
+    console.log(`Region ${process.env.CDK_DEFAULT_REGION}`);
+    console.log(`Deploying to ${process.env.CDK_DEFAULT_ACCOUNT}`);
+
   }
 }
